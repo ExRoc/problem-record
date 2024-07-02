3aa9938e8f45f5a2ab20ba113b1240323c1db0d5
ExRoc <guanzhanpeng.ExRoc@gmail.com>
2024-07-03 04:19:36 +0800

    add Codeforces 912 div.2 C

---
+++ Codeforces/Codeforces-Round-912-(Div.2)/C.Theofanis'-Nightmare.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long LL;
+const int maxn = 100000 + 100;
+int T, n;
+LL ans;
+LL num[maxn];
+
+int main() {
+#ifdef ExRoc
+    freopen("test.txt", "r", stdin);
+#endif // ExRoc
+
+    cin >> T;
+    while (T--) {
+        cin >> n;
+        for (int i = 1; i <= n; ++i) {
+            cin >> num[i];
+        }
+        ans = 0;
+        num[n + 1] = 0;
+        for (int i = n; i >= 1; --i) {
+            num[i] += num[i + 1];
+            if (num[i] >= 0) {
+                ans += num[i];
+            }
+        }
+        if (num[1] <= 0) {
+            ans += num[1];
+        }
+        cout << ans << endl;
+    }
+
+    return 0;
+}
