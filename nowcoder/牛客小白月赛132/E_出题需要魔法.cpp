ac7bd358ea2440c8a3018512205abe7c45e15a11
ExRoc <1595038502@qq.com>
2026-05-08 21:08:55 +0800

    update 牛客小白月赛132

--- nowcoder/牛客小白月赛132/E_出题需要魔法.cpp
+++ nowcoder/牛客小白月赛132/E_出题需要魔法.cpp
@@ -1,60 +1,53 @@
 #include <bits/stdc++.h>
 using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
-template <typename T>
+template <class S, S (*op)(S, S)>
 class SparseTable {
    public:
-    SparseTable(const vector<T>& arr)
-        : n_(arr.size()), log2_(n_ + 1), st_min_(), st_max_() {
+    SparseTable(const vector<S>& arr) : n_(arr.size()), log2_(n_ + 1), st_() {
         log2_[1] = 0;
         for (int i = 2; i <= n_; ++i) {
             log2_[i] = log2_[i / 2] + 1;
         }
 
         int k = log2_[n_] + 1;
-        st_min_.assign(k, vector<T>(n_));
-        st_max_.assign(k, vector<T>(n_));
+        st_.assign(k, vector<S>(n_));
 
         for (int i = 0; i < n_; ++i) {
-            st_min_[0][i] = arr[i];
-            st_max_[0][i] = arr[i];
+            st_[0][i] = arr[i];
         }
 
         for (int j = 1; j < k; ++j) {
             int step = 1 << (j - 1);
             for (int i = 0; i + (1 << j) <= n_; ++i) {
-                st_min_[j][i] =
-                    min(st_min_[j - 1][i], st_min_[j - 1][i + step]);
-                st_max_[j][i] =
-                    max(st_max_[j - 1][i], st_max_[j - 1][i + step]);
+                st_[j][i] = op(st_[j - 1][i], st_[j - 1][i + step]);
             }
         }
     }
 
-    T QueryMin(int l, int r) const {
-        int j = log2_[r - l + 1];
-        return min(st_min_[j][l], st_min_[j][r - (1 << j) + 1]);
-    }
-
-    T QueryMax(int l, int r) const {
-        int j = log2_[r - l + 1];
-        return max(st_max_[j][l], st_max_[j][r - (1 << j) + 1]);
+    S Query(int l, int r) const {
+        int len = r - l;
+        int j = log2_[len];
+        return op(st_[j][l], st_[j][r - (1 << j)]);
     }
 
    private:
     int n_;
     vector<int> log2_;
-    vector<vector<T>> st_min_;
-    vector<vector<T>> st_max_;
+    vector<vector<S>> st_;
 };
 
-long long findL(int idx, const SparseTable<int>& st, int x) {
+int op(int a, int b) { return max(a, b); }
+
+long long findL(int idx, const SparseTable<int, op>& st, int x) {
     int low = 0;
     int high = idx;
     int mid;
     while (high - low > 1) {
         mid = (high + low) >> 1;
-        if (st.QueryMax(mid, idx) <= x) {
+        if (st.Query(mid, idx + 1) <= x) {
             high = mid;
         } else {
             low = mid;
@@ -63,13 +56,13 @@ long long findL(int idx, const SparseTable<int>& st, int x) {
     return high;
 }
 
-long long findR(int idx, const SparseTable<int>& st, int x, int n) {
+long long findR(int idx, const SparseTable<int, op>& st, int x, int n) {
     int low = idx;
     int high = n + 1;
     int mid;
     while (high - low > 1) {
         mid = (high + low) >> 1;
-        if (st.QueryMax(idx, mid) <= x) {
+        if (st.Query(idx, mid + 1) <= x) {
             low = mid;
         } else {
             high = mid;
@@ -90,7 +83,7 @@ int main() {
         for (int i = 1; i <= n; ++i) {
             cin >> num[i];
         }
-        SparseTable<int> st(num);
+        SparseTable<int, op> st(num);
         for (int i = 1; i <= n; ++i) {
             long long l = findL(i, st, num[i]);
             long long r = findR(i, st, num[i], n);
